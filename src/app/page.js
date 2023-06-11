f2eb45495c9eb26e49a6bca0d4e36c9dc1c4d122
lazizbeksultonov <lazizbeksultonov0804@gmail.com>
2023-06-11 14:45:09 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className={'ml-24 flex bg-gradient-conic text-white'}></div>
+    </main>
+  )
+}
